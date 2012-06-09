0d9522dd37837a27b1bf6be71183e7f1319d94ec
Rico P <MailToRico@googlemail.com>
2012-06-09 15:06:32 +0200

    changed map structure

--- src/game.levelmanager.js
+++ src/game.levelmanager.js
@@ -9,61 +9,63 @@ if(!GAME_LEVELMANAGER_JS) {
 GAME.LEVELMANAGER = {}; 
 
 GAME.LEVELMANAGER.loadlevel = function(name, gl, callbackprogress, callbackfinished) {
-	var mappath = "maps/" + name + ".json"; 
+	var mapfilepath = "maps/" + name + ".json"; 
 	var mainprogram = null; 
 	var aVertex = -1; 
 	var aTextureuv = -1; 
 	var aNormal = -1; 
 
 	GLT.loadmanager.loadFiles({
-		"files" : [ mappath ], 
+		"files" : [ mapfilepath ], 
 		"error" : function (file, m) {
 			console.error(file, m); 
 		}, 
 		"finished" : function(files) {
-			var mapdata = files[mappath]; 
+			var mapdata = files[mapfilepath]; 
 			processLevel(mapdata, callbackprogress, function(mapdata) {
 				//TODO 
 			});  
 		}
 	}); 
 
-	function processLevel(map, onupdate, onfinished) {
+	function processLevel(mapdata, onupdate, onfinished) {
 		GLT.loadmanager.loadFiles({
-			"files" : map.data, 
+			"files" : mapdata.files, 
 			"update" : onupdate, 
 			"error" : function (file, m) {
 				console.error(file, m); 
 			}, 
-			"finished" : function(data) {
-				console.log("loaded"); 
-				var dict = mapTagToKey(data, gl); 				
+			"finished" : function(filesWithNames) {
+				var files = mapNameToKey(filesWithNames, gl); 				
 
-				mainprogram = dict[map.programname];
+				mainprogram = GLT.SHADER.compileProgram( gl, files[mapdata.programname] );
 				aVertex     = gl.getAttribLocation(mainprogram, "aVertex"); 
 				aTextureuv  = gl.getAttribLocation(mainprogram, "aTextureuv"); 
 				aNormal     = gl.getAttribLocation(mainprogram, "aNormal"); 
 
 				var gameobjects = [];
-				for(var i = 0; i != map.objects.models.length; i++) {
-					var entity = map.objects.models[i]; 
+				for(var i = 0; i != mapdata.objects.models.length; i++) {
+					var entity = mapdata.objects.models[i]; 
 					var name = entity.name; 
-					var objdata = dict[entity.model];
-					var texture = dict[entity.texture]; 
-					var program = dict[entity.program]; 
+					var objdata = files[entity.model];
+					var texture = files[entity.texture]; 
+					var program = files[entity.program]; 
 
 					gameobjects.push( createPropperGameObject({
 						"name"    : entity.name, 
-						"objdata" : dict[entity.model],
-						"texture" : dict[entity.texture], 
-						"program" : dict[entity.program]  
+						"objdata" : files[entity.model],
+						"texture" : files[entity.texture], 
+						"program" : files[entity.program]  
 					}));
 				}
 
-				var grid = [[]]; 
-
+				var grid = getGrid(mapdata); 
 			}
 		});
+	}
+
+	function getGrid(mapdata) {
+		
 	}
 
 	function createPropperGameObject(info) {
@@ -80,28 +82,23 @@ GAME.LEVELMANAGER.loadlevel = function(name, gl, callbackprogress, callbackfinis
 		gl.bindTexture(gl.TEXTURE_2D, null); 
 
 		return {
-			"name" : info.name,
-			"schema" : info.objdata.schema, 
-			"buffer" : vtnBuffer, 
-			"aVertex" : aVertex, 
+			"name"       : info.name,
+			"schema"     : info.objdata.schema, 
+			"buffer"     : vtnBuffer, 
+			"aVertex"    : aVertex, 
 			"aTextureuv" : aTextureuv, 
-			"aNormal" : aNormal, 
-			"texture" : texture
+			"aNormal"    : aNormal, 
+			"texture"    : texture
 		};
 	}
 
-	function mapTagToKey(data) {
+	function mapNameToKey(data) {
 		var dict = {};
 		var d; 
 		for(var k in data) {
 			d = data[k]; 
-			if(d.tag) {
-				if(d.type === "program") { 
-					dict[d.tag] = GLT.SHADER.compileProgram(gl, d.data); 
-				}
-				else { 
-					dict[d.tag] = d.data;
-				}
+			if(d.name) {
+				dict[d.name] = d.data;
 			}
 		}
 		return dict; 
